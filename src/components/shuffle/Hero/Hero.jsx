b06152649e83bb24cea55f83a8b6d69b88e4047a
Tom Clark <tom@octue.com>
2021-02-22 18:41:54 +0000

    IMP Background image optimised for desktop

--- src/components/shuffle/Hero/Hero.jsx
+++ src/components/shuffle/Hero/Hero.jsx
@@ -3,16 +3,14 @@ import React from 'react'
 import Container from '@material-ui/core/Container'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
-
 import Box from '@material-ui/core/Box'
 
-import { makeStyles } from '@material-ui/core'
+import { makeStyles, useTheme } from '@material-ui/core'
+import { graphql, useStaticQuery } from 'gatsby'
+import BackgroundImage from 'gatsby-background-image'
 
 const useStyles = makeStyles((theme) => ({
-  section: {
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: 'cover',
-    backgroundColor: theme.palette.primary.dark,
+  container: {
     paddingTop: theme.spacing(12),
     paddingBottom: theme.spacing(12),
     [theme.breakpoints.up('md')]: {
@@ -38,48 +36,74 @@ const useStyles = makeStyles((theme) => ({
   },
 }))
 
-function Hero(props) {
+function Hero() {
   const classes = useStyles()
+
+  const theme = useTheme()
+  const data = useStaticQuery(
+    graphql`
+      query {
+        desktop: file(relativePath: { eq: "backgrounds/tinted-hex.jpg" }) {
+          childImageSharp {
+            fluid(quality: 90, maxWidth: 1920) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+      }
+    `
+  )
+
+  const imageData = data.desktop.childImageSharp.fluid
+
   return (
-    <section
-      className={classes.section}
-      style={{
-        backgroundImage:
-          'url("https://shuffle.dev/projects/uploads/files/d8/d8d7c281d5c0768a1418c9e5add3183668edd40c/tinted-hex.original.jpg")',
-      }}
-    >
-      <Container maxWidth="md">
-        <Box textAlign="center" color="common.white">
-          <Typography variant="h2" component="h2" gutterBottom={true}>
-            <Typography color="secondary" variant="h2" component="span">
-              Science we can build on{' '}
+    <Box component="section" height="100vh">
+      <BackgroundImage
+        Tag="div"
+        fluid={imageData}
+        /* See https://github.com/timhagn/gatsby-background-image/issues/142
+         * for why these styles are applied here.
+         * Unlike a dynamic react site, this shouldn't be a performance issue.
+         */
+        style={{
+          backgroundColor: theme.palette.background,
+          width: '100%',
+          height: '100%',
+        }}
+      >
+        <Container maxWidth="md" className={classes.container}>
+          <Box textAlign="center" color="common.white">
+            <Typography variant="h2" component="h2" gutterBottom={true}>
+              <Typography color="secondary" variant="h2" component="span">
+                Science we can build on{' '}
+              </Typography>
             </Typography>
-          </Typography>
-          <Container maxWidth="sm">
-            <Typography
-              variant="subtitle1"
-              color="textSecondary"
-              paragraph={true}
-              className={classes.description}
-            >
-              Octue helps scientists take full advantage of data, with fewer
-              frustrations. Our building blocks make your data groundwork
-              easier, quicker and more sustainable, setting you free to focus on
-              the science.
-            </Typography>
-          </Container>
-          <Box mt={3}>
-            <Button
-              variant="contained"
-              color="secondary"
-              className={classes.primaryAction}
-            >
-              Let's build something together
-            </Button>
+            <Container maxWidth="sm">
+              <Typography
+                variant="subtitle1"
+                color="textSecondary"
+                paragraph={true}
+                className={classes.description}
+              >
+                Octue helps scientists take full advantage of data, with fewer
+                frustrations. Our building blocks make your data groundwork
+                easier, quicker and more sustainable, setting you free to focus
+                on the science.
+              </Typography>
+            </Container>
+            <Box mt={3}>
+              <Button
+                variant="contained"
+                color="secondary"
+                className={classes.primaryAction}
+              >
+                Let's build something together
+              </Button>
+            </Box>
           </Box>
-        </Box>
-      </Container>
-    </section>
+        </Container>
+      </BackgroundImage>
+    </Box>
   )
 }
 
