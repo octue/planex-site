c84fb634a5fa0708a1e089310e40a24f57f78378
Tom Clark <tom@octue.com>
2021-03-12 13:14:06 +0000

    IMP Subscribe form component

---
+++ src/components/forms/SubscribeForm/SubscribeForm.jsx
@@ -0,0 +1,116 @@
+import React, { useEffect } from 'react'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { useForm } from 'react-hook-form'
+import { makeStyles } from '@material-ui/core/styles'
+import TextField from '@material-ui/core/TextField'
+import Typography from '@material-ui/core/Typography'
+import LinearProgress from '@material-ui/core/LinearProgress'
+import Button from '@material-ui/core/Button'
+import Box from '@material-ui/core/Box'
+import * as Yup from 'yup'
+import EmailIcon from '@material-ui/icons/Email'
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Invalid email address')
+    .required('Must be a valid email address'),
+})
+
+const useStyles = makeStyles((theme) => ({
+  box: {
+    [theme.breakpoints.down('xs')]: {
+      width: '100%',
+    },
+  },
+  button: {
+    marginTop: `16px`,
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(2), // Set to align height with the input
+      paddingTop: '15px',
+      paddingBottom: '15px',
+    },
+    [theme.breakpoints.down('xs')]: {
+      width: '100%',
+    },
+  },
+  progress: {
+    height: theme.spacing(0.5),
+    marginBottom: `${theme.spacing(1)}px`,
+  },
+}))
+
+export default ({ onSubmit, apiErrors, submitting }) => {
+  const { register, handleSubmit, setError, clearErrors, errors } = useForm({
+    resolver: yupResolver(schema),
+  })
+
+  const classes = useStyles()
+
+  useEffect(() => {
+    clearErrors()
+    Object.keys(apiErrors).map((key) => {
+      setError(key, {
+        type: 'api',
+        message: apiErrors[key],
+      })
+    })
+  }, [apiErrors])
+
+  const helperText = {
+    nonFieldErrors: errors.nonFieldErrors?.message || null,
+    email:
+      errors.email?.message ||
+      "We promise we won't bug you, or sell your data to third parties",
+  }
+
+  const hasError = {
+    nonFieldErrors: !!errors?.nonFieldErrors,
+    email: !!errors?.email,
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Box width="100%" mb={submitting ? 0 : 0.5}>
+        {submitting && (
+          <LinearProgress color="primary" className={classes.progress} />
+        )}
+        {hasError['nonFieldErrors'] && (
+          <Typography color="error" variant="subtitle2">
+            {helperText.nonFieldErrors}
+          </Typography>
+        )}
+      </Box>
+      <Box width="100%" display="flex" flexWrap="wrap">
+        <Box flexGrow={1} className={classes.box}>
+          <TextField
+            defaultValue=""
+            disabled={submitting}
+            error={hasError['email']}
+            fullWidth
+            helperText={helperText['email']}
+            id="email"
+            inputRef={register}
+            label="Email *"
+            margin="normal"
+            name="email"
+            variant="outlined"
+          />
+        </Box>
+        <Box className={classes.box}>
+          <Button
+            className={classes.button}
+            variant="contained"
+            fullwidth
+            disabled={submitting}
+            endIcon={<EmailIcon />}
+            color="primary"
+            size="large"
+            type="submit"
+          >
+            Subscribe
+          </Button>
+        </Box>
+      </Box>
+    </form>
+  )
+}
---
+++ src/components/forms/SubscribeForm/SubscribeForm.stories.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import SubscribeForm from './SubscribeForm'
+import AxiosMock from '../../../../.storybook/AxiosMock'
+import FormHandler from '../../../services/FormHandler'
+
+export default {
+  title: 'Forms/Subscribe',
+  component: SubscribeForm,
+  parameters: { actions: { argTypesRegex: '^on.*' } },
+}
+
+const Template = (args) => {
+  const handleSubmit = (data) => {
+    console.log('onSubmit data:', data)
+  }
+  return <SubscribeForm {...args} onSubmit={handleSubmit} />
+}
+
+export const NotSubmitted = Template.bind({})
+NotSubmitted.args = {
+  apiErrors: {},
+  submitting: false,
+}
+
+export const Submitting = Template.bind({})
+Submitting.args = {
+  apiErrors: {},
+  submitting: true,
+}
+
+export const WithSubmissionErrors = Template.bind({})
+WithSubmissionErrors.args = {
+  apiErrors: {
+    nonFieldErrors: 'The server got unhealthy and barfed all over itself. Ugh.',
+    email: 'We think this email is dodgy',
+  },
+  submitting: false,
+}
+
+export const WithAxiosMock = (args) => {
+  const mock = (apiMock) => {
+    // Using the testing panel of Google Cloud Functions, you can try out the functions and get real responses.
+    // I've copied some here, adapted for comedy and we can use that to mock up an erroneous response from the API.
+    apiMock
+      .onPost('https://europe-west1-planex.cloudfunctions.net/subscribe')
+      .reply((config) => {
+        const error = new Error('Request failed')
+        error.config = config
+        error.response = {
+          status: 400,
+          data: {
+            nonFieldErrors: 'Something overarchingly terrible happened',
+            email: 'This is terrible email. What, jobs@apple.com was taken?',
+          },
+          headers: [],
+          config: config,
+        }
+        return Promise.reject(error)
+      })
+  }
+  return (
+    <AxiosMock mock={mock}>
+      <FormHandler
+        FormComponent={SubscribeForm}
+        endpoint="/subscribe"
+        {...args}
+      />
+    </AxiosMock>
+  )
+}
+WithAxiosMock.args = {}
