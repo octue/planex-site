849830425a5c73c87c42054fbaa90e500e014e9a
Tom Clark <tom@octue.com>
2021-03-14 09:37:42 +0000

    IMP CTA dialog attached to main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,7 @@ import HereToHelp from '../components/shuffle/HereToHelp/HereToHelp'
 import TheTeam from '../components/shuffle/TheTeam/TheTeam'
 import OurMission from '../components/shuffle/OurMission/OurMission'
 import { darkTheme } from '../themes'
+import { SubscribeForm, CallToAction } from '../containers'
 
 export default function Index() {
   const navBarProps = {
@@ -21,6 +22,7 @@ export default function Index() {
 
   return (
     <BasicPage navBarProps={navBarProps} footerProps={footerProps}>
+      <CallToAction FormComponent={SubscribeForm} />
       <ThemeProvider theme={darkTheme}>
         <Hero />
       </ThemeProvider>
