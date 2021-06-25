898e8ebf782e913de27121db788ccac365c3a19c
Tom Clark <tom@octue.com>
2021-06-25 14:13:23 +0100

    REF: Cleared out multiple footers

--- src/components/elements/Footer/Footer.jsx
+++ src/components/elements/Footer/Footer.jsx
@@ -1,36 +1,84 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Grid from '@material-ui/core/Grid'
+import { makeStyles } from '@material-ui/core/styles'
+import Container from '@material-ui/core/Container'
 
-import MicroFooter from './MicroFooter'
-import SmallFooter from './SmallFooter'
-import BigFooter from './BigFooter'
+import Copyright from '../Copyright'
+import GradientStripe from '../GradientStripe'
+import { Link } from '../../core/Link'
 
-/**
- * Footer
- *
- * A dark themed footer for the site. Provides multiple different kinds.
- */
-function Footer({ kind, ...rest }) {
-  switch (kind) {
-    case 'micro':
-      return <MicroFooter {...rest} />
-    case 'small':
-      return <SmallFooter {...rest} />
-    case 'big':
-      return <BigFooter {...rest} />
-    default:
-      return null
-  }
+import { FaTwitter, FaLinkedin, FaGithub } from 'react-icons/fa'
+
+const useStyles = makeStyles((theme) => ({
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+  },
+  button: {
+    margin: theme.spacing(1),
+    fontSize: '24px',
+  },
+  copyright: {
+    fontSize: `${theme.spacing(1.5)}px`,
+  },
+}))
+
+function SmallFooter({ id }) {
+  const classes = useStyles()
+  return (
+    <footer id={id} className={classes.footer}>
+      <GradientStripe />
+      <Container>
+        <Grid
+          container
+          direction="row"
+          justify="space-between"
+          alignItems="center"
+        >
+          <Grid item>
+            <Copyright className={classes.copyright} />
+          </Grid>
+          <Grid item>
+            <Link
+              href="https://twitter.com/octue"
+              kind="external"
+              componentType="iconButton"
+              className={classes.button}
+              aria-label="twitter"
+            >
+              <FaTwitter />
+            </Link>
+            <Link
+              href="https://linkedin.com/company/octue"
+              kind="external"
+              componentType="iconButton"
+              className={classes.button}
+              aria-label="linkedin"
+            >
+              <FaLinkedin />
+            </Link>
+            <Link
+              href="https://github.com/octue"
+              kind="external"
+              componentType="iconButton"
+              className={classes.button}
+              aria-label="github"
+            >
+              <FaGithub />
+            </Link>
+          </Grid>
+        </Grid>
+      </Container>
+    </footer>
+  )
 }
 
-Footer.defaultProps = {
+SmallFooter.defaultProps = {
   id: 'footer',
-  kind: 'small',
 }
 
-Footer.propTypes = {
-  kind: PropTypes.oneOf(['big', 'small', 'micro', 'none']),
+SmallFooter.propTypes = {
   id: PropTypes.string,
 }
 
-export default Footer
+export default SmallFooter
---
+++ src/components/elements/Footer/FooterLayout.stories.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import Box from '@material-ui/core/Box'
+import Footer from './Footer'
+
+export default {
+  title: 'Elements/Footer',
+  component: Footer,
+}
+
+const Template = ({ ...args }) => (
+  // This flexbox arrangement is the simplest way of getting the footer to stick to the bottom of the page,
+  // but not cover content that is longer than the page.
+  <Box
+    display="flex"
+    flexDirection="column"
+    justifyContent="space-between"
+    style={{ minHeight: '100vh' }}
+  >
+    <Box component="main">
+      <Box
+        style={{
+          backgroundColor: '#6d9fbb',
+          width: '100vw',
+          height: `${args.height}vh`,
+        }}
+      >
+        This story shows how to lay out a footer with flex. This is the actual
+        content of the page. Adjust the height in the control to > 100 to see
+        what happens when page content is longer than the viewport height.
+        Switch between kinds in the controls to see different footers.
+      </Box>
+    </Box>
+    <Footer />
+  </Box>
+)
+
+export const Layout = Template.bind({})
+Layout.args = {
+  height: 50,
+}
