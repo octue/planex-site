7820402002c289b28c41b4408f911b4529b2d2ca
Tom Clark <tom@octue.com>
2021-06-25 17:27:01 +0100

    FEA: Add LogoLink core element

---
+++ src/components/core/Link/LogoLink.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+import { makeStyles, useTheme } from '@material-ui/core'
+import { Link } from '../../core'
+
+const useStyles = makeStyles((theme) => ({
+  logo: {
+    height: '36px',
+  },
+}))
+
+function LogoLink({ logoClasses, ...rest }) {
+  const classes = useStyles()
+  const theme = useTheme()
+  const logoClassName = classNames(classes.logo, logoClasses)
+  return (
+    <Link {...rest}>
+      <img src={theme.logos.text.dark} alt="logo" className={logoClassName} />
+    </Link>
+  )
+}
+
+LogoLink.defaultProps = {
+  href: '/',
+}
+
+LogoLink.propTypes = {
+  /**
+   * The href to where the logo links
+   */
+  href: PropTypes.string,
+}
+
+export default LogoLink
