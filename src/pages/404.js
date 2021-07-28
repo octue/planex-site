b0e5af29db8e4d765fcd6a008bee9c80c991c9d6
Tom Clark <tom@octue.com>
2021-07-28 09:06:05 +0100

    FEA: SEO object now accepts extra page and site meta

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,7 +4,7 @@ import SEO from '../components/core/SEO'
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
+    <SEO pageMeta={{title="Not Found"}} />
     <h1>404: Not Found</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
--- src/pages/about.js
+++ src/pages/about.js
@@ -6,7 +6,7 @@ import SEO from '../components/core/SEO'
 
 const About = () => (
   <Layout>
-    <SEO title="Page two" />
+    <SEO pageMeta={{title="Page two"}} />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
