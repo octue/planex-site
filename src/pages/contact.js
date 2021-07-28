00abbe7a3f5a7ce6239613ff1b0b13e7d3c83daa
Tom Clark <tom@octue.com>
2021-07-28 23:29:31 +0100

    FEA: Add data queries to About and Contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,25 +1,31 @@
 import React from 'react'
+import { HelmetDatoCms } from 'gatsby-source-datocms'
+import { graphql } from 'gatsby'
 import Typography from '@material-ui/core/Typography'
 import Container from '@material-ui/core/Container'
 import BasicPage from '../containers/layout/BasicPage'
-
-import SEO from '../components/core/SEO'
 import { ContactForm } from '../containers/forms'
 
-export default function Contact() {
-  const footerProps = {
-    kind: 'big',
+export const query = graphql`
+  query ContactPageQuery {
+    page: datoCmsContactPage {
+      seoMetaTags {
+        ...GatsbyDatoCmsSeoMetaTags
+      }
+    }
   }
+`
 
+export default function Contact({ data }) {
   return (
-    <BasicPage offset pt={6} pb={8} px={2} footerProps={footerProps}>
-      <SEO pageMeta={{ title: 'Contact' }} />
+    <BasicPage offset pt={6} pb={8} px={2}>
+      <HelmetDatoCms seo={data.page.seoMetaTags} />
       <Container maxWidth="sm">
         <Typography gutterBottom variant="h4" color="primary">
-          Contact Us
+          {data.page.heading}
         </Typography>
         <Typography variant="subtitle1" color="textSecondary">
-          Drop us a message, we'd love to hear what you're working on...
+          {data.page.subheading}
         </Typography>
         <ContactForm />
       </Container>
