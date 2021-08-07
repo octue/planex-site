69bed77c22e297c83d6028c8760e83fc15d06f09
Tom Clark <tom@octue.com>
2021-08-07 23:43:44 +0100

    CHO: Remove comments

--- src/components/sections/JoinTheMovement/JoinTheMovement.stories.jsx
+++ src/components/sections/JoinTheMovement/JoinTheMovement.stories.jsx
@@ -1,30 +1,12 @@
 import React from 'react'
 import JoinTheMovement from './JoinTheMovement'
 
-import { graphql, useStaticQuery } from 'gatsby'
-
 export default {
   title: 'Sections/JoinTheMovement',
   component: JoinTheMovement,
 }
 
-const Template = ({ ...args }) => {
-  // const data = useStaticQuery(
-  //   graphql`
-  //     query {
-  //       desktop: file(relativePath: { eq: "backgrounds/tinted-hex.jpg" }) {
-  //         childImageSharp {
-  //           fluid(quality: 90, maxWidth: 100) {
-  //             ...GatsbyImageSharpFluid_withWebp
-  //           }
-  //         }
-  //       }
-  //     }
-  //   `
-  // )
-
-  // const imageData = data.desktop.childImageSharp.fluid
-
+const Template = () => {
   return <JoinTheMovement />
 }
 
