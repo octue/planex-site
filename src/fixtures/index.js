8265bce666be99e71a49bcee95217be1cc326396
Tom Clark <tom@octue.com>
2021-08-20 09:53:11 +0100

    FEA: Added fixture for team section

---
+++ src/fixtures/index.js
@@ -0,0 +1,1 @@
+export { team } from './sections/team.js'
---
+++ src/fixtures/sections/team.js
@@ -0,0 +1,102 @@
+export const team = {
+  people: [
+    {
+      bio:
+        'Tom architected Octue’s twined framework and has been awarded patents related to AI and intelligent control of wind farms. He’s worked in renewables since 2006 and loves it.',
+      github: 'https://www.github.com/thclark',
+      avatar: {
+        gatsbyImageData: {
+          images: {
+            sources: [],
+            fallback: {
+              src:
+                'https://www.datocms-assets.com/53117/1628495637-thc-oas-photo.jpg?auto=format&crop=focalpoint&fit=crop&fp-x=0.47&fp-y=0.37&h=150&w=150',
+              srcSet:
+                'https://www.datocms-assets.com/53117/1628495637-thc-oas-photo.jpg?auto=format&crop=focalpoint&dpr=0.26&fit=crop&fp-x=0.47&fp-y=0.37&h=150&w=150 38w,\nhttps://www.datocms-assets.com/53117/1628495637-thc-oas-photo.jpg?auto=format&crop=focalpoint&dpr=0.5&fit=crop&fp-x=0.47&fp-y=0.37&h=150&w=150 75w,\nhttps://www.datocms-assets.com/53117/1628495637-thc-oas-photo.jpg?auto=format&crop=focalpoint&fit=crop&fp-x=0.47&fp-y=0.37&h=150&w=150 150w',
+              sizes: '(min-width: 150px) 150px, 100vw',
+            },
+          },
+          layout: 'constrained',
+          placeholder: {
+            fallback:
+              'data:image/jpg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAoHBwgHBgoREwgVFQ0NEhINDg0NGhUNDQ0NFxUZGBYVFhUaHysjHR0oHRUiMDUlKC0vMjIyGSI4PTcwPCsxMi8BCgsLDg0OEw8OHS8oFhwvLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vL//AABEIABgAGAMBIgACEQEDEQH/xAAYAAEBAQEBAAAAAAAAAAAAAAAABgUEAv/EAB8QAAIBBAIDAAAAAAAAAAAAAAABAgMEETEFIRIVQf/EABcBAQADAAAAAAAAAAAAAAAAAAIAAQP/xAAYEQADAQEAAAAAAAAAAAAAAAAAAQIxIf/aAAwDAQACEQMRAD8Aqb24im+zPjdR8tnDyN61nsxfZNVNkYp6iyhcxa2CaocnlbAjOtPHINtMnq+YzALeChincSj9AAUGtP/Z',
+          },
+          width: 150,
+          height: 150,
+        },
+        alt: 'Mugshot of Tom Clark',
+        title: 'Tom Clark',
+      },
+      name: 'Tom Clark',
+      role: 'CEO, Founder',
+      linkedin: 'https://www.linkedin.com/thclark1',
+      twitter: 'https://twitter.com/tom_at_octue',
+    },
+    {
+      bio:
+        'Andy has 20+ years of experience delivering complex projects in areas where people, the environment, and energy interact. Andy leads the WindForS cluster of 25 members at 7 locations.',
+      github: 'https://github.com/AndyClifton',
+      avatar: {
+        gatsbyImageData: {
+          images: {
+            sources: [],
+            fallback: {
+              src:
+                'https://www.datocms-assets.com/53117/1629394851-female-avatar-300x300-01.jpeg?auto=format&crop=focalpoint&fit=crop&fp-x=0.5&fp-y=0.5&h=150&w=150',
+              srcSet:
+                'https://www.datocms-assets.com/53117/1629394851-female-avatar-300x300-01.jpeg?auto=format&crop=focalpoint&dpr=0.26&fit=crop&fp-x=0.5&fp-y=0.5&h=150&w=150 38w,\nhttps://www.datocms-assets.com/53117/1629394851-female-avatar-300x300-01.jpeg?auto=format&crop=focalpoint&dpr=0.5&fit=crop&fp-x=0.5&fp-y=0.5&h=150&w=150 75w,\nhttps://www.datocms-assets.com/53117/1629394851-female-avatar-300x300-01.jpeg?auto=format&crop=focalpoint&fit=crop&fp-x=0.5&fp-y=0.5&h=150&w=150 150w',
+              sizes: '(min-width: 150px) 150px, 100vw',
+            },
+          },
+          layout: 'constrained',
+          placeholder: {
+            fallback:
+              'data:image/jpg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAoHBwgHBgoIEQgLFgoVDh0QGA0XDh0NFh0YIx8fJCIfHRYqKy0vJilAKRUmNTspMjkvPj5KGSI4PUM6PDc9MjsBCgsLBQUFEAUFEC8cFhwvLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vL//AABEIABgAGAMBIgACEQEDEQH/xAAZAAADAAMAAAAAAAAAAAAAAAAABgcBBAX/xAAiEAABAwMEAwEAAAAAAAAAAAAAAQIDBAUREiExQRQkgQb/xAAUAQEAAAAAAAAAAAAAAAAAAAAA/8QAFBEBAAAAAAAAAAAAAAAAAAAAAP/aAAwDAQACEQMRAD8Ar9WmadwvMi9j6d2vnZFAu4usro/I5TkBkpG4YgGvS1jFYm5kBUv15dFTOdnomkX7xi3ZYMuxq06usgAFBtd6dLC1c9AAAf/Z',
+          },
+          width: 150,
+          height: 150,
+        },
+        alt: 'Default Avatar',
+        title: 'Default Avatar ',
+      },
+      name: 'Andy Clifton',
+      role: 'Co-Founder, Strategic Advisor',
+      linkedin: 'https://www.linkedin.com/in/andyclifton/',
+      twitter: '',
+    },
+    {
+      bio:
+        "A physicist by training, Marcus leads development of Octue's main OSS libraries, SDK and Twined. He is responsible for developing our Digital Twin technology beyond the cutting edge.",
+      github: 'https://github.com/cortadocodes',
+      avatar: {
+        gatsbyImageData: {
+          images: {
+            sources: [],
+            fallback: {
+              src:
+                'https://www.datocms-assets.com/53117/1629394851-female-avatar-300x300-01.jpeg?auto=format&crop=focalpoint&fit=crop&fp-x=0.5&fp-y=0.5&h=150&w=150',
+              srcSet:
+                'https://www.datocms-assets.com/53117/1629394851-female-avatar-300x300-01.jpeg?auto=format&crop=focalpoint&dpr=0.26&fit=crop&fp-x=0.5&fp-y=0.5&h=150&w=150 38w,\nhttps://www.datocms-assets.com/53117/1629394851-female-avatar-300x300-01.jpeg?auto=format&crop=focalpoint&dpr=0.5&fit=crop&fp-x=0.5&fp-y=0.5&h=150&w=150 75w,\nhttps://www.datocms-assets.com/53117/1629394851-female-avatar-300x300-01.jpeg?auto=format&crop=focalpoint&fit=crop&fp-x=0.5&fp-y=0.5&h=150&w=150 150w',
+              sizes: '(min-width: 150px) 150px, 100vw',
+            },
+          },
+          layout: 'constrained',
+          placeholder: {
+            fallback:
+              'data:image/jpg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAoHBwgHBgoIEQgLFgoVDh0QGA0XDh0NFh0YIx8fJCIfHRYqKy0vJilAKRUmNTspMjkvPj5KGSI4PUM6PDc9MjsBCgsLBQUFEAUFEC8cFhwvLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vL//AABEIABgAGAMBIgACEQEDEQH/xAAZAAADAAMAAAAAAAAAAAAAAAAABgcBBAX/xAAiEAABAwMEAwEAAAAAAAAAAAAAAQIDBAUREiExQRQkgQb/xAAUAQEAAAAAAAAAAAAAAAAAAAAA/8QAFBEBAAAAAAAAAAAAAAAAAAAAAP/aAAwDAQACEQMRAD8Ar9WmadwvMi9j6d2vnZFAu4usro/I5TkBkpG4YgGvS1jFYm5kBUv15dFTOdnomkX7xi3ZYMuxq06usgAFBtd6dLC1c9AAAf/Z',
+          },
+          width: 150,
+          height: 150,
+        },
+        alt: 'Default Avatat',
+        title: 'Default Avatar ',
+      },
+      name: 'Marcus Lugg',
+      role: 'Senior Developer, Twined/SDK',
+      linkedin: '',
+      twitter: '',
+    },
+  ],
+  heading: 'Our Team',
+  subheading: 'The team. The squad. The Teamsquad.',
+}
