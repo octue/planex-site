a8947fe6de3812fce6817dc131bc3e1cc1c280de
Tom Clark <tom@octue.com>
2021-09-03 18:26:58 +0100

    FIX: Attempt to fix gatsby cloud issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
     title: 'Octue',
   },
   plugins: [
-    `gatsby-plugin-gatsby-cloud`,
+    // `gatsby-plugin-gatsby-cloud`,
     'gatsby-plugin-sitemap',
     'gatsby-plugin-robots-txt',
     'gatsby-plugin-react-helmet',
